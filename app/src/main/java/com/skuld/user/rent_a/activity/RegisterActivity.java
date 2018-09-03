8927d3c69b0e1ac9bb2c10a596a5b68a18ebad6a
Raymarkim Cabrera <kim.cabrera@coreproc.ph>
2018-09-04 00:14:18 +0800

    Added signup with facebook

--- app/src/main/java/com/skuld/user/rent_a/activity/RegisterActivity.java
+++ app/src/main/java/com/skuld/user/rent_a/activity/RegisterActivity.java
@@ -5,10 +5,20 @@ import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.FacebookSdk;
+import com.facebook.GraphRequest;
+import com.facebook.GraphResponse;
+import com.facebook.login.LoginManager;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.firestore.DocumentReference;
@@ -19,6 +29,10 @@ import com.skuld.user.rent_a.presenter.RegisterPresenter;
 import com.skuld.user.rent_a.utils.GeneralUtils;
 import com.skuld.user.rent_a.views.RegisterView;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -27,6 +41,7 @@ import butterknife.OnClick;
 
 public class RegisterActivity extends BaseActivity implements RegisterView {
 
+    private static final String TAG = RegisterActivity.class.getSimpleName();
     @BindView(R.id.firstNameEditText)
     EditText mFirstNameEditText;
 
@@ -51,6 +66,7 @@ public class RegisterActivity extends BaseActivity implements RegisterView {
     private String firstName, middleName, lastName, email, password, confirmPassword, contactNumber;
     private RegisterPresenter mRegisterPresenter;
     private Context mContext;
+    private CallbackManager callbackManager;
 
     public static Intent newIntent(Context context) {
         Intent intent = new Intent(context, RegisterActivity.class);
@@ -61,9 +77,64 @@ public class RegisterActivity extends BaseActivity implements RegisterView {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+
+        FacebookSdk.sdkInitialize(getApplicationContext());
+        callbackManager = CallbackManager.Factory.create();
+
         mContext = this;
 
         initPresenter();
+
+        initFacebookSignUp();
+    }
+
+    private void initFacebookSignUp() {
+        LoginButton mLoginButton = (LoginButton) findViewById(R.id.signUpFacebookButton);
+        mLoginButton.setReadPermissions(Arrays.asList("email", "first_name", "last_name"));
+        mLoginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+
+
+                GraphRequest request = GraphRequest.newMeRequest(
+                        loginResult.getAccessToken(),
+                        new GraphRequest.GraphJSONObjectCallback() {
+                            @Override
+                            public void onCompleted(JSONObject jsonObject,
+                                                    GraphResponse response) {
+                                try {
+                                    mRegisterPresenter.registerUser(jsonObject.getString("first_name"),
+                                            "",
+                                            jsonObject.getString("last_name"),
+                                            jsonObject.getString("email"),
+                                            "",
+                                            "");
+                                } catch (JSONException e) {
+                                    e.printStackTrace();
+                                }
+
+                            }
+                        });
+
+                Bundle parameters = new Bundle();
+                parameters.putString("fields", "id,first_name,last_name,email");
+                request.setParameters(parameters);
+                request.executeAsync();
+            }
+
+
+            @Override
+            public void onCancel() {
+                Log.d(TAG, "Login attempt cancelled.");
+            }
+
+            @Override
+            public void onError(FacebookException e) {
+                e.printStackTrace();
+                Log.d(TAG, "Login attempt failed.");
+                LoginManager.getInstance().logOut();
+            }
+        });
     }
 
     @Override
@@ -75,7 +146,7 @@ public class RegisterActivity extends BaseActivity implements RegisterView {
         mRegisterPresenter = new RegisterPresenter(mContext, this);
     }
 
-    @OnClick({R.id.signUpButton, R.id.loginTextView})
+    @OnClick({R.id.signUpButton, R.id.loginTextView, R.id.signUpGmailButton})
     void onClick(View view) {
         switch (view.getId()) {
             case R.id.signUpButton:
